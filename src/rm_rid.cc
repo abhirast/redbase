832dc1f4bdb945eb6db68e23082f9d55645bed9e
abhirast <arastogi@stanford.edu>
2015-04-08 13:45:38 -0700

    Implemented RM_RID

--- src/rm_rid.cc
+++ src/rm_rid.cc
@@ -5,15 +5,31 @@
 using namespace std;
 
 // Default constructor
-RID::RID();
+RID::RID() {
+	pageNum = -1;
+	slotNum = -1;
+}
 
-RID::RID(PageNum pageNum, SlotNum slotNum);
+RID::RID(PageNum pageNum, SlotNum slotNum) {
+	this->pageNum = pageNum;
+	this->slotNum = slotNum;
+}
 
 // Destructor
-RID::~RID();
+RID::~RID(){}
 
 // Return page number
-RC RID::GetPageNum(PageNum &pageNum) const;
+RC RID::GetPageNum(PageNum &pageNum) const {
+	// Error if RID not initialized using second constructor
+	if (this->pageNum < 0) return RM_PAGE_UNINIT;
+	pageNum = this->pageNum;
+	return OK_RC;
+}
 
 // Return slot number
-RC RID::GetSlotNum(SlotNum &slotNum) const;
+RC RID::GetSlotNum(SlotNum &slotNum) const {
+	//Error if RID not initialized using second constructor
+	if (this->slotNum < 0) return RM_SLOT_UNINIT;
+	slotNum = this->slotNum;
+	return OK_RC;	
+}
