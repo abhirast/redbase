8b99a149d0dc5a8f32f3c6b9f1e4172a14bdd3a4
abhirast <arastogi@stanford.edu>
2015-04-11 19:43:00 -0700

    Implemented and debugged

--- src/rm_record.cc
+++ src/rm_record.cc
@@ -4,12 +4,26 @@
 
 using namespace std;
 
-RM_Record::RM_Record();
-RM_Record::~RM_Record();
+RM_Record::RM_Record() {
+	bIsAllocated = 0;
+}
+
+RM_Record::~RM_Record() {
+	// free the dynamically allocated memory
+	if (bIsAllocated) delete[] record;
+}
 
 // Return the data corresponding to the record.  Sets *pData to the
 // record contents.
-RC RM_Record::GetData(char *&pData) const;
+RC RM_Record::GetData(char *&pData) const {
+	if (!bIsAllocated) return RM_INVALID_RECORD;
+	pData = record;
+	return OK_RC;
+}
 
 // Return the RID associated with the record
-RM_Record::RC GetRid (RID &rid) const;
+RC RM_Record::GetRid (RID &rid) const{
+	if (!bIsAllocated) return RM_INVALID_RECORD;
+	rid = this->rid;
+	return OK_RC;
+}
