7ee7f770902884af57459fb7bde364e93bab57fc
abhirast <arastogi@stanford.edu>
2015-04-11 19:41:46 -0700

    Debugger rm_rid

--- src/rm_rid.cc
+++ src/rm_rid.cc
@@ -16,12 +16,14 @@ RID::RID(PageNum pageNum, SlotNum slotNum) {
 }
 
 // Destructor
-RID::~RID(){}
+RID::~RID(){
+	// Nothing to do
+}
 
 // Return page number
 RC RID::GetPageNum(PageNum &pageNum) const {
 	// Error if RID not initialized using second constructor
-	if (this->pageNum < 0) return RM_PAGE_UNINIT;
+	if (this->pageNum < 0) return RM_INVALID_RID;
 	pageNum = this->pageNum;
 	return OK_RC;
 }
@@ -29,7 +31,7 @@ RC RID::GetPageNum(PageNum &pageNum) const {
 // Return slot number
 RC RID::GetSlotNum(SlotNum &slotNum) const {
 	//Error if RID not initialized using second constructor
-	if (this->slotNum < 0) return RM_SLOT_UNINIT;
+	if (this->slotNum < 0) return RM_INVALID_RID;
 	slotNum = this->slotNum;
 	return OK_RC;	
 }
--- src/rm_rid.h
+++ src/rm_rid.h
@@ -7,10 +7,15 @@
 #ifndef RM_RID_H
 #define RM_RID_H
 
+
 // We separate the interface of RID from the rest of RM because some
 // components will require the use of RID but not the rest of RM.
 
 #include "redbase.h"
+
+#ifndef RM_INVALID_RID
+#define RM_INVALID_RID              (START_RM_WARN + 6)
+#endif
 
 //
 // PageNum: uniquely identifies a page in a file
@@ -30,11 +35,15 @@ public:
     RID();                                         // Default constructor
     RID(PageNum pageNum, SlotNum slotNum);
     ~RID();                                        // Destructor
-
+    RID(const RID &rid);						   // Copy constructor
+    RID& operator=(const RID &rid);				   // Assignment operator
     RC GetPageNum(PageNum &pageNum) const;         // Return page number
     RC GetSlotNum(SlotNum &slotNum) const;         // Return slot number
 
 private:
+	// negative values indicate absence of attribute
+	PageNum pageNum;
+	SlotNum slotNum;
 };
 
 #endif
